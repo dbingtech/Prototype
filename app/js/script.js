61608a9aa07f69c1c4c980dafad36cf278ee753f
Dan Bingman <techfromtheteacher@gmail.com>
2024-03-13 07:16:48 -0400

    update 3/12

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,10 @@
 
 function focusSearchField (){
     const searchfield = document.getElementsByClassName("search-field").focus();
-    
 }
+
+const copyrightSpan = document.getElementById('copyright');
+const newDate = new Date();
+let thisYear = newDate.getFullYear();
+copyrightSpan.innerHTML = thisYear;
+console.log(copyrightSpan);
