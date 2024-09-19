eddaa453cc787bd5bbceda0ddc12d2808bdab8b2
Dan Bingman <dbingman@bdi-usa.com>
2024-09-19 11:55:58 -0400

    updates previous to 9/18/24

--- app/js/script.js
+++ app/js/script.js
@@ -8,3 +8,18 @@ const newDate = new Date();
 let thisYear = newDate.getFullYear();
 copyrightSpan.innerHTML = thisYear;
 console.log(copyrightSpan);
+
+// ----------------Search Button drop-down show options
+
+
+document.getElementById("search-dropdown").addEventListener("click", searchButton);
+    
+function searchButton(){    
+    if(document.getElementById("search-options").style.display == "none"){
+        document.getElementById("search-options").style.display = "block";    
+        document.getElementById("search-options").style.visibility = "visible";
+    } else {
+        document.getElementById("search-options").style.display = "none";
+        document.getElementById("search-options").style.visibility = "hidden";
+    }
+}
