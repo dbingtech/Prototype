65d9309be6f1933cea70174856dd017656c2710e
Dan Bingman <techfromtheteacher@gmail.com>
2026-02-18 14:38:42 -0500

    New Version

--- app/js/script.js
+++ app/js/script.js
@@ -1,25 +1,50 @@
+// Load components
+// Load header
+        fetch('header.html')
+            .then(response => response.text())
+            .then(data => document.getElementById('header').innerHTML = data);
+        
+        // Load nav
+        fetch('nav.html')
+            .then(response => response.text())
+            .then(data => document.getElementById('nav').innerHTML = data);
+        
+        // Load footer
+        fetch('footer.html')
+            .then(response => response.text())
+            .then(data => document.getElementById('footer').innerHTML = data);
+        // Load hero
+        fetch('hero.html')
+            .then(response => response.text())
+            .then(data => document.getElementById('hero').innerHTML = data);
+        // Load home categories
+        fetch('home_categories.html')
+            .then(response => response.text())
+            .then(data => document.getElementById('home_categories').innerHTML = data);
 
 function focusSearchField (){
     const searchfield = document.getElementsByClassName("search-field").focus();
 }
 
-const copyrightSpan = document.getElementById('copyright');
-const newDate = new Date();
+let copyrightSpan = document.getElementById('copyright');
+let newDate = new Date();
 let thisYear = newDate.getFullYear();
 copyrightSpan.innerHTML = thisYear;
-console.log(copyrightSpan);
+// console.log(copyrightSpan);
 
 // ----------------Search Button drop-down show options
 
 
-document.getElementById("search-dropdown").addEventListener("click", searchButton);
+// document.getElementById("search-dropdown").addEventListener("click", searchButton);
     
-function searchButton(){    
-    if(document.getElementById("search-options").style.display == "none"){
-        document.getElementById("search-options").style.display = "block";    
-        document.getElementById("search-options").style.visibility = "visible";
-    } else {
-        document.getElementById("search-options").style.display = "none";
-        document.getElementById("search-options").style.visibility = "hidden";
-    }
-}
+// function searchButton(){    
+//     if(document.getElementById("search-options").style.display == "none"){
+//         document.getElementById("search-options").style.display = "block";    
+//         document.getElementById("search-options").style.visibility = "visible";
+//     } else {
+//         document.getElementById("search-options").style.display = "none";
+//         document.getElementById("search-options").style.visibility = "hidden";
+//     }
+// }
+
+
